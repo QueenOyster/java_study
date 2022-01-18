eb383f63f8c97901e55067f69b3dd235df4f7b84
DESKTOP-17R30V6\david <nekoder777@gmail.com>
2022-01-18 15:08:48 +0900

    2022-01-18

---
+++ kr/bit/Book.java
@@ -0,0 +1,12 @@
+package kr.bit;
+
+//책(객체=구조=덩어리): 제목 ,가격, 출판사, 페이지수...(상태정보) + (행위정보: 동작 = 메서드)
+public class Book { // UDDT
+	// class란 사용자 정의 자료형을 만드는 도구, 설계도
+	// 데이터 구조 Book VO - value object, Book DTO - data Transfer object
+	public String title;
+	public int price;
+	public String company;
+	public int page;
+
+}
