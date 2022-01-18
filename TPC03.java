397eafa289e6428faeab12d0c2c34216287b801f
DESKTOP-17R30V6\david <nekoder777@gmail.com>
2022-01-18 15:56:49 +0900

    2022-01-18

--- TPC03.java
+++ TPC03.java
@@ -1,4 +1,5 @@
 import kr.bit.Book;
+import kr.bit.PersonVO;
 public class TPC03 {
 
 	public static void main(String[] args) {
@@ -6,12 +7,32 @@ public class TPC03 {
 		// 정수 1개를 저장하기 위한 변수를 선언하시요.
 		int a;
 		a = 10;
+		System.out.println(a);
 		
 		// 책 1권을 저장하기 위한 변수를 선언하시오.
 		Book b;
+		b = new Book();
+		b.title = "자바";
+		b.price = 15000;
+		b.company = "한빛미디어";
+		b.page = 700;
 		
+		System.out.print(b.title+"\t");
+		System.out.print(b.price+"\t");
+		System.out.print(b.company+"\t");
+		System.out.println(b.page);
 		
-
+		PersonVO p; // 각기 다른 데이터 타입이 저장되므로 배열이 아니다.
+		p = new PersonVO();
+		p.name = "김사람";
+		p.age = 40;
+		p.weight = 68.4f;
+		p.height = 175.7f;
+		
+		System.out.print(p.name + "\t");
+		System.out.print(p.age + "\t");
+		System.out.print(p.weight + "\t");
+		System.out.println(p.height);
 	}
 
 }
