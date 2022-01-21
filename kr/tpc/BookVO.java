227aa73ac0d792f6a63ef206551fb82a279601af
DESKTOP-17R30V6\david <nekoder777@gmail.com>
2022-01-21 23:43:10 +0900

    Java Constructor Rules(Default, Overload)

--- kr/tpc/BookVO.java
+++ kr/tpc/BookVO.java
@@ -6,6 +6,22 @@ public class BookVO {
 	public int price;
 	public String company;
 	public int page;
-	// 디폴트 생성자 메서드(생략)
-
+	// 생성자가 없다 -> 디폴트 생성자 메서드는 작성하지 않아도 자동 생성됨
+	// 생성자가 있다 -> 디폴트 생성자 메서드는 자동 생성되지 않음
+	public BookVO() {
+		// 초기화 작업
+		// this 생략 가능
+		this.title = "자바";
+		this.price = 14000;
+		this.company = "이지스";
+		this.page = 780;
+	}
+	
+	// 생성자 메서드의 중복정의(overloading)
+	public BookVO(String title, int price, String company, int page) {
+		this.title = title;
+		this.price = price;
+		this.company = company;
+		this.page = page;
+	}
 }
