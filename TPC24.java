51ae8fd789a9fbf979d9d84c0e88fe7d70790377
DESKTOP-17R30V6\david <nekoder777@gmail.com>
2022-01-26 00:17:25 +0900

    Polymorphism and Array

---
+++ TPC24.java
@@ -0,0 +1,36 @@
+import kr.tpc.*;
+
+public class TPC24 {
+
+	public static void main(String[] args) {
+		// 2. 다형성 배열
+		// Dog, Cat 저장 할 배열을 생성하시오.
+
+		Animal[] ani = new Animal[2];
+		ani[0] = new Dog();
+		ani[1] = new Cat();
+
+		for (int i = 0; i < ani.length; i++) {
+			ani[i].eat();
+			if (ani[i] instanceof Cat) {
+				((Cat) ani[1]).night();
+			}
+
+		} // for문의 끝
+
+		display(ani);
+
+	}
+
+	private static void display(Animal[] ani) { // 다형성 배열
+		for (int i = 0; i < ani.length; i++) {
+			ani[i].eat();
+			if (ani[i] instanceof Cat) {
+				((Cat) ani[1]).night();
+			}
+
+		} // for문의 끝
+
+	}
+
+}
